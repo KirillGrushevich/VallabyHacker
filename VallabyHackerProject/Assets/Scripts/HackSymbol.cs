ad1ac7e3058a413ae191ff8a9b440d23857c003b
BooLee-inc <boolee@tut.by>
2017-03-25 00:05:13 +0300

    New scripts

---
+++ VallabyHackerProject/Assets/Scripts/HackSymbol.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum HackSymbol {
+
+	Element1,
+	Element2,
+	Element3,
+	Element4,
+	Element5,
+	Element6,
+	Element7,
+	Element8,
+	Element9,
+	Element10,
+	Element11,
+	Element12,
+	Element13,
+	Element14,
+	Element15,
+}
---
+++ VallabyHackerProject/Assets/Scripts/SymbolBase.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class SymbolBase : MonoBehaviour {
+
+	protected abstract void Init ();
+
+	protected abstract void SetSymbol ();
+
+	protected abstract bool IsEquals();
+		
+}
