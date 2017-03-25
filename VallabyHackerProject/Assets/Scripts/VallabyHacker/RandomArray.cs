7535d3f803be8d6c7f2f584e4b2af5e7d514e5f0
BooLee-inc <boolee@tut.by>
2017-03-25 15:48:17 +0300

    Random Array Script

---
+++ VallabyHackerProject/Assets/Scripts/VallabyHacker/RandomArray.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+public static class RandomArray 
+{
+	public static bool[] GetRandomArray(int length)
+	{
+		int[] randomIntArray = new int[length];
+
+		for (int i = 0; i < length; i++) {
+			randomIntArray [i] = Random.Range (0, 2);
+		}
+
+		bool[] randomBoolArray = new bool[length];
+
+		for (int i = 0; i < length; i++) 
+		{
+			if (randomIntArray [i] == 0) 
+			{
+					randomBoolArray [i] = false;
+			} 
+			else 
+			{
+					randomBoolArray [i] = true;
+			}
+
+			if (i > 1 && (randomBoolArray [i - 2] == randomBoolArray [i - 1]) && (randomBoolArray [i - 1] == randomBoolArray[i])) 
+			{
+				randomBoolArray [i] = !randomBoolArray [i];
+			}
+		}
+
+		return randomBoolArray;
+	}
+}
