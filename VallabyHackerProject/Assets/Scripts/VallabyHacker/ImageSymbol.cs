0b72d3fb5a99dd936f94f07a78e214ec8e12b456
BooLee-inc <boolee@tut.by>
2017-03-25 10:27:01 +0300

    Image class

--- VallabyHackerProject/Assets/Scripts/VallabyHacker/ImageSymbol.cs
+++ VallabyHackerProject/Assets/Scripts/VallabyHacker/ImageSymbol.cs
@@ -1,12 +1,16 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ImageSymbol : SymbolBase {
+
+	public Sprite[] sprites;
 
 	public override void SetSymbol (HackSymbol symbol)
 	{
 		base.SetSymbol (symbol);
 
+		GetComponent<Image> ().sprite = sprites[(int)symbol];
 	}
 }
