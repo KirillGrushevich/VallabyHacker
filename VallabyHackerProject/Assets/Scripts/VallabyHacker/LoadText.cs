c587769bdba5ad10489341c4d16d25fd97a522cd
BooLee-inc <boolee@tut.by>
2017-03-25 12:37:00 +0300

    Make load text

---
+++ VallabyHackerProject/Assets/Scripts/VallabyHacker/LoadText.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LoadText : MonoBehaviour {
+
+	private Text text;
+	private string loadText = "Good news evereone!!!\n" +
+		"__________Loading________\n" +
+		"I make his load stuff!!\n" +
+		"Yyyyyyyeeepi!!\n" +
+		"asdr34543&@sefjfs_23!!\n" +
+		"23456_34543+@4535\n"+
+		"sefwerrt345534\n"+
+		"Seft test is complited";
+
+	void Start () 
+	{
+		text = GetComponent<Text> ();
+		StartCoroutine (AnimateText ());
+	}
+
+	IEnumerator AnimateText()
+	{
+		for (int i = 0; i < loadText.Length; i++) 
+		{
+			text.text = loadText.Substring (0, i);
+			yield return new WaitForSeconds (.03f);
+		}
+	}
+
+}
