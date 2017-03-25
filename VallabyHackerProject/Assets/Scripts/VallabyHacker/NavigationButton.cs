3e4b50d59f9d57ff0b5abcb1591466c293555319
BooLee-inc <boolee@tut.by>
2017-03-25 16:59:00 +0300

    button

---
+++ VallabyHackerProject/Assets/Scripts/VallabyHacker/NavigationButton.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NavigationButton : MonoBehaviour {
+
+	public void OnClick(float dir)
+	{
+		GameSwipeDetection.SwipeAction.Invoke (dir);
+	}
+}
